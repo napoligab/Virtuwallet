70751fba6f0a075eaa06ea90e90ddc3a2c614fa6
“Brenda <brndglopes@gmail.com>
2022-08-08 15:12:41 +0100

    new views

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,8 +1,24 @@
+const isLoggedIn = require("../middleware/isLoggedIn");
+
 const router = require("express").Router();
 
-router.get("/dashboard", (req, res, next) => {
+router.get("/dashboard", isLoggedIn, (req, res, next) => {
     res.render("dashboard");
 });
+
+router.get("/dashboard/:userId", isLoggedIn, (req, res, next) => {
+    res.render("dashboard", {user});
+});
+
+router.post("/dashboard/:userId", isLoggedIn, (req, res, next) => {
+  const {firstName, lastName, email, password} = req.body;
+  const {userId} = req.params;
+  
+  
+
+    res.render("dashboard", );
+});
+
 
 module.exports = router;
 
