65f43c98ee0441d99b355a9b619fee8825580d6c
“Brenda <brndglopes@gmail.com>
2022-08-08 12:13:18 +0100

    user fixed

--- models/User.model.js
+++ models/User.model.js
@@ -2,15 +2,13 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
-<<<<<<< HEAD
-=======
     email: {
       type: String,
       trim: true,
       require: true,
       unique: true,
     },
->>>>>>> 55e607dd2d4c201ee6bd7530d27aab18d681216f
+
     firstName: {
       type: String,
       trim: true,
@@ -19,12 +17,6 @@ const userSchema = new Schema(
     lastName: {
       type: String,
       trim: true,
-    },
-    email: {
-      type: String,
-      trim: true,
-      require: [true, 'E-mail is required'],
-      unique: [true, 'This e-mail is already being used'],
     },
     password: {
       type: String,
