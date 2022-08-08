71b55d3a8ecd37b6e20a17d3a8eecdf4af149a76
Gabriella Hevesy <55456609+napoligab@users.noreply.github.com>
2022-08-08 12:39:07 +0100

    appjs correction

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app);
 
-const capitalized = require("./utils/capitalized");
-const projectName = "VirtWallet";
+const capitalized = require('./utils/capitalized');
+const projectName = 'VirtWallet';
 
 app.locals.appTitle = `${projectName}`;
 
 // 👇 Start handling routes here
-const index = require("./routes/index.routes");
-app.use("/", index);
+const index = require('./routes/index.routes');
+app.use('/', index);
 
-const signup = require("./routes/auth.routes");
-app.use("/signup", signup);
+// const signup = require('./routes/auth.routes');
+// app.use('/signup', signup);
 
-const login = require("./routes/auth.routes");
-app.use("/", login);
+// const login = require('./routes/auth.routes');
+// app.use('/login', login);
 
-const authRoutes = require("./routes/auth.routes");
-app.use("/", authRoutes);
+const authRoutes = require('./routes/auth.routes');
+app.use('/', authRoutes);
+
+const dashboard = require('./routes/auth.routes');
+app.use('/dashboard', dashboard);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
