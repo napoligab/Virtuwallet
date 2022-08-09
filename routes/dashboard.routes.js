99fa51f9908861642baebda69e43988625ac9747
Gabriella Hevesy <55456609+napoligab@users.noreply.github.com>
2022-08-09 11:53:57 +0100

    user and entry working

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -22,48 +22,27 @@ router.get('/new-entry', isLoggedIn, (req, res, next) => {
 });
 
 router.post('/new-entry', isLoggedIn, (req, res, next) => {
-  const { date, amount, category, location, description } = req.body;
+  const { date, amount, category, location, type } = req.body;
   const user = req.session.user;
-  Entry.create({ date, amount, category, location, description })
+  Entry.create({ date, amount, category, location, type })
     .then((newEntry) => {
-      User.findByIdAndUpdate(user._id, {
-        $push: { entries: newEntry._id },
-      });
+      return User.findByIdAndUpdate(
+        user._id,
+        {
+          $push: { entries: newEntry._id },
+        },
+        { new: true }
+      );
     })
-    .then(() => res.redirect(`/dashboard/${user._id}`))
+    .then((user) => {
+      res.redirect(`/dashboard/${user._id}`);
+    })
     .catch((err) => console.log('Error while creating an entry: ', err));
 });
-
-// router.post('/post-create', (req, res, next) => {
-//   const { title, content, author } = req.body;
-
-//   Post.create({ title, content, author })
-//     .then((newPost) => {
-//       return User.findByIdAndUpdate(author, { $push: { posts: newPost._id } });
-//     })
-//     .then(() => res.redirect('/posts'))
-//     .catch((err) => next(err));
-// });
-
-/* 
-movieRouter.post('/', (req, res, next) => {
-    Movie.create(req.body)
-    .then( newMovie => {
-      // console.log("New movie: ", newMovie);
-  
-      // res.redirect ALWAYS has '/' because it is the URL
-      res.redirect('/movies')
-    } )
-    .catch( err => console.log("Error while creating a movie: ", err))
-  }) */
 
 router.get('/edit-entry', isLoggedIn, (req, res, next) => {
   res.render('entries/edit-entry');
 });
-
-/* router.post("/new-entry", isLoggedIn, (req, res, next) => {
-    Entry.create({})
-}) */
 
 router.get('/edit-user/:userId', isLoggedIn, (req, res, next) => {});
 
